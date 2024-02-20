57bb361c2442476ef327806c8afc6d9a0ed38698
znehraks <znehraks@gmail.com>
2024-02-20 16:35:56 +0900

    husky

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import './App.css';
 function App() {
   const [count, setCount] = useState(0);
 
-  console.log('hiohihisssshsssihi');
+  console.log('hiohihishsssihi');
   return (
     <>
       <div>
