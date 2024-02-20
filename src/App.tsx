8399f41f56cd3718655ef02a7001f08fe6711a2a
znehraks <znehraks@gmail.com>
2024-02-20 16:41:23 +0900

    console.log 테스트

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import './App.css';
 function App() {
   const [count, setCount] = useState(0);
 
-  console.log('hiohihishsssihi');
+  console.log('hiohihishssasdsaasassihi');
   return (
     <>
       <div>
